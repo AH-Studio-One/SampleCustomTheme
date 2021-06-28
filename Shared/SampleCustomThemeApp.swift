a43cf351442e9576eccf9c27ef101eab2390d3fe
Alina Hambaryan <alina.hambaryan@doktor.se>
2021-06-28 16:26:26 +0200

    Initial Commit

---
+++ Shared/SampleCustomThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCustomThemeApp.swift
+//  Shared
+//
+//  Created by A H on 2021-06-28.
+//
+
+import SwiftUI
+
+@main
+struct SampleCustomThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
